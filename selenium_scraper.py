2433b6da2e782626d8945f0d326ab2c6fd58721d
kuba1302 <kuba1302@gmail.com>
2021-03-18 11:32:49 +0100

    everything works

--- selenium_scraper.py
+++ selenium_scraper.py
@@ -4,12 +4,18 @@ desired_width = 320
 pd.set_option('display.width', desired_width)
 from time import sleep
 import random
+from selenium.webdriver.chrome.options import Options
 
 class CoList:
     def __init__(self):
         self.path = r"C:\Users\Admin\Desktop\chromedriver.exe"
-        self.driver = webdriver.Chrome(self.path)
         self.driver.get('https://www.colist.eu/searchbygroup.php?lang=en&land_id=16&&group_id=15560&&org_group_id=0,15084,15560,&&records=10&')
+        self.options = webdriver.ChromeOptions()
+        self.options.headless = True
+        self.options.add_argument("--disable-extensions")
+        self.options.add_argument("--disable-dev-shm-usage")
+        self.options.add_argument("--no-sandbox")
+        self.driver = webdriver.Chrome(self.path, options=self.options)
 
     colist_data = pd.DataFrame(columns=['Company Name', 'info', 'loc_phone'])
 
@@ -65,7 +71,7 @@ class EuroPages:
         self.driver = webdriver.Chrome(self.path)
 
     def wait(self):
-        sleep(random.uniform(1, 1.5))
+        sleep(random.uniform(0.4, 0.6))
 
 
     def get_name(self):
@@ -77,6 +83,7 @@ class EuroPages:
 
     def get_number(self):
         try:
+            self.wait()
             number = self.driver.find_element_by_xpath(
                 '/html/body/div[2]/div[2]/aside/div[1]/div[1]/div/div[2]/div/div').text
             return number
@@ -86,7 +93,7 @@ class EuroPages:
     def get_website(self):
         try:
             number = self.driver.find_element_by_xpath(
-                '/html/body/div[2]/div[2]/aside/div[1]/div[1]/div/div[2]/div/div').text
+                '/html/body/div[2]/div[2]/aside/div[1]/div[1]/div/a[1]').get_attribute('href')
             return number
         except:
             return None
@@ -98,7 +105,7 @@ class EuroPages:
         except:
             return None
 
-    def get_adress(self):
+    def get_address(self):
         try:
             address = self.driver.find_element_by_xpath(
                 '/html/body/div[2]/div[2]/aside/div[1]/div[1]/div/div[1]/div/address/dl/span/span/dd[2]/pre').text
@@ -111,35 +118,52 @@ class EuroPages:
             self.driver.find_element_by_xpath('/html/body/div[2]/div[2]/aside/div[1]/div[1]/div/div[2]/div').click()
         except:
             pass
-    def click_company(self):
+
+    def get_info(self):
+        company_info = [self.get_name(), self.get_number(), self.get_website(),
+                        self.get_country(), self.get_address()]
+        print(company_info)
+        return company_info
+
+    animal_data = pd.DataFrame(columns=['Company Name', 'number', 'website', 'country', 'address'])
+
+    def next_company(self, i):
         try:
-            self.driver.find_element_by_xpath(
-                '/html/body/div[2]/div[3]/div[3]/ul/li[{}]/div[2]/div[2]/a'.format(i)).click()
+            link = self.driver.find_element_by_xpath(
+                '/html/body/div[2]/div[3]/div[3]/ul/li[{}]/div[2]/div[2]/a'.format(i)).get_attribute('href')
+            self.driver.get(link)
             self.wait()
-        except:
-            print('error')
-    def animal_production(self):
-        website = 'https://www.europages.co.uk/companies/results.html?ih=02559&ih=02540H&ih=01607A&ih=02715B&ih=02540A'
-        self.driver.get(website)
-
-        animal_data = pd.DataFrame(columns=['Company Name', 'number', 'website', 'country', 'address'])
-
-        for i in range (1, 34):
-            if i != 5:
-                try:
-                    self.driver.find_element_by_xpath('/html/body/div[2]/div[3]/div[3]/ul/li[{}]/div[2]/div[2]/a'.format(i)).click()
-                    self.wait()
-                except:
-                    print('error')
-
-            self.wait()
-            self.driver.back()
-            animal_data.loc[len(animal_data.index)] = [name, number, website, country, address]
+            self.click_phone()
             print(i)
-            print([name, number, website, country, address])
+            self.animal_data.loc[len(self.animal_data.index)] = self.get_info()
+            self.driver.back()
+        except:
+            try:
+                link = self.driver.find_element_by_xpath(
+                    '/html/body/div[2]/div[3]/div[3]/ul/li[{}]/div[2]/div/a'.format(i)).get_attribute('href')
+                self.driver.get(link)
+                self.wait()
+                self.click_phone()
+                print(i)
+                self.animal_data.loc[len(self.animal_data.index)] = self.get_info()
+                self.driver.back()
+            except:
+                print('error')
 
-        animal_data.to_csv('animal.csv', index=False)
-        print(animal_data)
+    def animal_production(self):
+        website = 'https://www.europages.co.uk/companies/results.html?ih=02740P&ih=02740Q&ih=02745R&ih=02700I&ih=02740G&ih=02701G&ih=02900D&ih=02745A&ih=02740J&ih=02745E&ih=02740R&ih=02740O&ih=' \
+                  '02740K&ih=02740C&ih=02740I&ih=02740A&ih=02701B&ih=02664B&ih=02703A&ih=02700A&ih=02745P&ih=02740N&ih=02700H&ih=02700B&ih=02740M&ih=02740F&ih=02664A&ih=02701A&ih=02703B&ih=02740H&ih=02740E'
+        self.driver.get(website)
+        for url_i in range(1, 278):
+            self.wait()
+            self.driver.get('https://www.europages.co.uk/companies/pg-{}/results.html?ih=02700B;02900D;02664A;02'
+                            '745A;02701G;02740O;02700A;02740Q;02703A;02701B;02740E;02740I;02745R;02664B;02700H;02740A;02701A;02740N;02740'
+                            'F;02703B;02740H;02700I;02745E;02740C;02740K;02740R;02740M;02740G;02745P;02740P;02740J'.format(url_i))
+            for i in range(1, 34):
+                self.next_company(i)
+
+        self.animal_data.to_csv('drinks_all_countries.csv', index=False)
+        self.driver.close()
 b = EuroPages()
 b.animal_production()
 
